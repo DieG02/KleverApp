0d4ca1291870c21660523edf8dea643a373a4c24
DieG02 <diegob0505@gmail.com>
2025-08-26 11:30:54 +0200

    [new] add commands and debug with GoogleSignIn

--- src/config/GoogleServices.ts
+++ src/config/GoogleServices.ts
@@ -1,13 +1,18 @@
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Config from 'react-native-config';
 
-GoogleSignin.configure({
-  scopes: [
-    // 'https://www.googleapis.com/auth/user.birthday.read',
-    // 'https://www.googleapis.com/auth/user.gender.read',
-    // 'https://www.googleapis.com/auth/user.addresses.read',
-  ],
-  webClientId: process.env.WEB_CLIENT_ID,
-  offlineAccess: true,
-});
+try {
+  GoogleSignin.configure({
+    scopes: [
+      // 'https://www.googleapis.com/auth/user.birthday.read',
+      // 'https://www.googleapis.com/auth/user.gender.read',
+      // 'https://www.googleapis.com/auth/user.addresses.read',
+    ],
+    webClientId: Config.WEB_CLIENT_ID,
+    offlineAccess: true,
+  });
+} catch (error: any) {
+  console.error('❌ Google configure error:', error.code, error.message);
+}
 
 export default GoogleSignin;
