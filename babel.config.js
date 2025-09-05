aa6a381452b47924e49aefabbb6b293197b21867
DieG02 <diegob0505@gmail.com>
2025-09-05 17:01:02 +0200

    [test] Setup testing environment

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
   plugins: [
     [
       'module:react-native-dotenv',
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|@react-navigation' +
+      '|@react-native-firebase' +
+      '|@react-native-google-signin' +
+      '|@react-native-voice' +
+      '|@react-native-async-storage' +
+      '|react-native-gesture-handler' +
+      '|react-native-progress' +
+      ')/)',
+  ],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    './jest.setup.js',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
